7d57df25c1f4397ddda6f699b1dfea0049a7e79c
hhoopes <heidih@gmail.com>
2016-02-24 21:16:15 -0700

    Add tests and functionality for admin create, index, destroy

--- test/features/admin_tools_test.rb
+++ test/features/admin_tools_test.rb
@@ -1,25 +1,36 @@
 require 'test_helper'
 
-class AdminCanEditAnExistingToolTest <ActionDispatch::IntegrationTest
+class AdminCanAddViewDeleteToolsTest <ActionDispatch::IntegrationTest
 
-  test "admin can edit an existing tool" do
-
-    tool = Tool.create(name: "Screwdriver", quantity: 30, price: 1999)
-    user = User.create(username: "JoeUser", password: "123", role: 0)
-    user.tools << tool
-
+  test "admin can add an existing tool" do
     admin = User.create(username: "admin", password: "123", role: 1)
     ApplicationController.any_instance.stubs(:current_user).returns(admin)
 
-    visit edit_admin_tool_path(tool)
+    visit new_admin_tool_path
 
     fill_in "Name", with: "Wrench"
     fill_in "tool[quantity]", with: "3" #can use either label or name
     fill_in "Price", with: "2000"
 
-    click_link_or_button "Update Tool"
+    click_link_or_button "Create Tool"
 
+    tool = Tool.last
     assert_equal current_path, admin_tool_path(tool)
+    assert page.has_content?("Wrench")
   end
 
+  test "admin can view all tools" do
+
+    user = User.create(username: "JillUser", password: "123", role: 0)
+    user.tools << Tool.create(name: "jackhammer", price: 33, quantity: 3)
+    admin = User.create(username: "admin", password: "123", role: 1)
+    admin.tools << Tool.create(name: "rasp", price: 33, quantity: 3)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+    visit admin_tools_path
+
+    assert page.has_content?("jackhammer")
+    assert page.has_content?("rasp")
+  end
 end
